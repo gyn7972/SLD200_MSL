cdd4f00bbc5b497fd0274c37f0a4a2007877bead
yngoo <yngoo@iqmc.co.kr>
2025-04-27 10:25:05 +0900

    레이저 드릴링 타이머 중지 코드 주석 처리
    `timer_ScannerCalibration.Stop();` 코드가 주석 처리되어 레이저 드릴링이 시작되지 않았을 때 타이머가 중지되지 않도록 변경되었습니다. 주석으로 남겨두어 필요 시 다시 사용할 수 있도록 하였습니다.

--- QMC.Common/Modules/WorkStage.cs
+++ QMC.Common/Modules/WorkStage.cs
@@ -7495,7 +7495,7 @@ namespace QMC.Common.Modules
                 if (!m_LaserDrillingWork_Start)
                 {
                     Console.WriteLine("Laser Drilling is not started.");
-                    timer_ScannerCalibration.Stop(); // 타이머 중지
+                    //timer_ScannerCalibration.Stop(); // 타이머 중지
                     SetRecoveraryLaserDrilling_MainStep(m_nLaserDrilling_MainStep);
                     return;
                 }
