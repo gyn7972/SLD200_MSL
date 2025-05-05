5c790cd94baa35b2af848c661214afabfb54c683
chseong <chseong@iqmc.co.kr>
2025-05-05 15:20:05 +0900

    M-Aligner 및 Work Stage 조건 로직 수정
    M-Aligner에서 모듈 픽업 조건을 기존 `workStage.m_bMainWorkCycle_Complete` 확인에서 `DryRun` 및 `DrillingRun` 상태의 단계(`Step`) 확인으로 변경하였습니다.
    Work Stage에서 모듈 Put Down 조건에 `unloader.m_nUnloader_Transfer_Step` 및 `unloader.m_nUnloaderTransferMoveType` 확인 로직을 추가하고, `DryRun` 및 `DrillingRun` 상태의 단계 확인 조건을 추가하였습니다.
    또한, 조건 이동 및 재배치를 통해 코드 가독성과 논리적 흐름을 개선하였으며, 변경된 로직에 맞춰 주석을 업데이트하였습니다.

--- QMC.Common/Modules/Loader.cs
+++ QMC.Common/Modules/Loader.cs
@@ -3746,14 +3746,11 @@ namespace QMC.Common.Modules
                     (m_nMAlign_Step == (int)MAlign_Step.None) &&
                     (m_bMAlign_Complete || m_bMAlign_Retry) &&
 
-                    !workStage.m_bMainWorkCycle_Complete &&                                                   //  Work Stage 의 완료 상태가 False 일 때 얼라인 완료된 모듈을 픽업 한다. 
-                    //!m_bLoader_Transfer_ModulePutDowntoWorkStage_Complete &&                                //  Unloader 가 Work Stage 의 모듈을 가져가면, M-Aligner 에서 얼라인 완료된 모듈을 픽업 한다. 
-                    //  Laser Drilling Cycle 이 완료되면 그때 Pick Up 하도록 한다.
-                    //((workStage.m_bMainWorkCycle_DryRun && workStage.m_bDryRun_Complete) ||                 //  Dry Run 이면?? Dry Run 완료 확인
-                    //(!workStage.m_bMainWorkCycle_DryRun && workStage.m_bLaserDrilling_Complete)) &&         //  Drilling Run 이면?? Drilling 완료 확인
+                    ////  M-Aligner 에서 Module 을 미리 Pick Up 하기 위해서 5번 조건으로 이동
+                    //!workStage.m_bMainWorkCycle_Complete &&                                                   //  Work Stage 의 완료 상태가 False 일 때 얼라인 완료된 모듈을 픽업 한다. 
+                    //((workStage.m_bMainWorkCycle_DryRun && (workStage.m_nDryRun_Step == (int)WorkStage.DryRun_Step.None)) ||                        //  Dry Run 이면?? Dry Run Step None 확인
+                    //(!workStage.m_bMainWorkCycle_DryRun && (workStage.m_nLaserDrilling_MainStep == (int)WorkStage.LaserDrilling_Step.None))) &&     //  Drilling Run 이면?? Drilling Step None 확인
 
-                    ((workStage.m_bMainWorkCycle_DryRun && (workStage.m_nDryRun_Step == (int)WorkStage.DryRun_Step.None)) ||                        //  Dry Run 이면?? Dry Run Step None 확인
-                    (!workStage.m_bMainWorkCycle_DryRun && (workStage.m_nLaserDrilling_MainStep == (int)WorkStage.LaserDrilling_Step.None))) &&     //  Drilling Run 이면?? Drilling Step None 확인
                     (m_nLoaderTransfer_ProcessStep == (int)LoaderTransferProcessStep.LoaderStep_ModulePickUp_MAligner) &&
                     m_bMAlignZone_ModuleExist)
                 {
@@ -3765,8 +3762,17 @@ namespace QMC.Common.Modules
                 // 5. Work Stage에 제품 안착
                 else if (m_bAUTORUN_Loader_Transfer_ModulePickUpfromMAligner_Complete &&
                     !m_bAUTORUN_Loader_Transfer_ModulePutDowntoWorkStage_Complete &&
+
                     (unloader.m_nUnloader_Transfer_Step == (int)Unloader_Transfer_Step.None) &&
                     (m_nLoaderTransfer_ProcessStep == (int)LoaderTransferProcessStep.LoaderStep_ModulePutDown_Stage) &&
+
+                    //  M-Aligner 에서 Module 을 미리 Pick Up 하기 위해서 4번 조건에 있던 것으로 5번으로 이동
+                    !workStage.m_bMainWorkCycle_Complete &&                                                   //  Work Stage 의 완료 상태가 False 일 때 얼라인 완료된 모듈을 픽업 한다. 
+                    ((workStage.m_bMainWorkCycle_DryRun && (workStage.m_nDryRun_Step == (int)WorkStage.DryRun_Step.None)) ||                        //  Dry Run 이면?? Dry Run Step None 확인
+                    (!workStage.m_bMainWorkCycle_DryRun && (workStage.m_nLaserDrilling_MainStep == (int)WorkStage.LaserDrilling_Step.None))) &&     //  Drilling Run 이면?? Drilling Step None 확인
+
+                    (unloader.m_nUnloaderTransferMoveType != (int)UnloaderTransferMoveType.Cycle_WorkStage_PickUp) && 
+
                     (workStage.m_nLaserDrilling_MainStep == (int)WorkStage.LaserDrilling_Step.None) &&      //  Work Stage 에서 아무것도 하지 않을 때
                     (workStage.m_nWorkStage_Move_Step == (int)WorkStage.WorkStage_Move_Step.None))
                 {
