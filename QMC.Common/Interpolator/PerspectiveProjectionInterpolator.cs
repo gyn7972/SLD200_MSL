849ff8d62d8484a118384432ed38c2f36e0d9fbf
youngnamkim <alli07kyn@gmail.com>
2025-05-06 10:54:38 +0900

    a

--- QMC.Common/Interpolator/PerspectiveProjectionInterpolator.cs
+++ QMC.Common/Interpolator/PerspectiveProjectionInterpolator.cs
@@ -2,6 +2,7 @@
 using QMC.Common.VisionPart;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Reflection;
 using System.Text;
@@ -11,8 +12,49 @@ namespace QMC.Common.Interpolator
 {
     public class PerspectiveProjectionInterpolator : BaseInterpolator
     {
+        class CacheData
+        {
+            public XyCoordinate LeftTop { get; set; }
+            public XyCoordinate RightBottom { get; set; }
+            public List<PositionOffset> Around4Point { get; set; }
+            public void AddAroundData(List<PositionOffset> around4Point)
+            {
+                Around4Point = around4Point;
+                if (around4Point.Count > 0)
+                {
+                    LeftTop = around4Point[0].Position;
+                    RightBottom = around4Point[2].Position;
+                }
+            }
+            public bool IsHit(XyCoordinate source)
+            {
+                if (Around4Point.Count == 0)
+                {
+                    return false;
+                }
+                double MinX = LeftTop.X;
+                double MinY = LeftTop.Y;
+                double MaxX = RightBottom.X;
+                double MaxY = RightBottom.Y;
+                if (source.X >= MinX && source.X <= MaxX && source.Y >= MinY && source.Y <= MaxY)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+        private CacheData cacheAround4Point = new CacheData();
+        private CacheData cacheDestAround4Point = new CacheData();
+
         protected List<PositionOffset> GetAround4Point(XyCoordinate source)
         {
+            if (cacheAround4Point.IsHit(source))
+            {
+                return cacheAround4Point.Around4Point;
+            }
             List<PositionOffset> list = new List<PositionOffset>();
             PositionOffset posLT = GetLeftTop(source);
             if (posLT.Position.X != 0 || posLT.Position.Y != 0)
@@ -34,11 +76,17 @@ namespace QMC.Common.Interpolator
             {
                 list.Add(posRB);
             }
+            cacheAround4Point.AddAroundData(list);
             return list;
         }
 
+
         protected List<PositionOffset> GetDestAround4Point(XyCoordinate dest)
         {
+            if (cacheDestAround4Point.IsHit(dest))
+            {
+                return cacheDestAround4Point.Around4Point;
+            }
             List<PositionOffset> list = new List<PositionOffset>();
             PositionOffset posLT = GetMeasureLeftTop(dest);
             if (posLT.Position.X != 0 || posLT.Position.Y != 0)
@@ -63,6 +111,8 @@ namespace QMC.Common.Interpolator
             return list;
         }
 
+        
+
         public override int Interpolate(XyCoordinate source, ref XyCoordinate dest)
         {
             int ret = 0;
@@ -79,9 +129,6 @@ namespace QMC.Common.Interpolator
                     coordinatesSource.Add(new XyCoordinate(v.Position.X, v.Position.Y));
                     coordinatesTarget.Add(new XyCoordinate(v.Position.X + v.Offset.X, v.Position.Y - v.Offset.Y));
 
-                    //  2025. 04. 17.  SCH : 로그파일 용량이 너무 커서..
-                    //Log.Write("PerspectiveProjectionInterpolator", String.Format("{0}, {1}, {2},{3},{4}",
-                    //    nIndex++, v.Position.X, v.Position.Y, v.Position.X + v.Offset.X, v.Position.Y - v.Offset.Y));
                 }
                 CorrectionMatrix = perspectiveProjection.projection_matrix(coordinatesSource, coordinatesTarget);
                 XyCoordinate t = perspectiveProjection.GetPerspectiveProjectionPoint(source, CorrectionMatrix);
@@ -93,9 +140,6 @@ namespace QMC.Common.Interpolator
                 dest = source;
             }
 
-            //  2025. 04. 17.  SCH : 로그파일 용량이 너무 커서..
-            //Log.Write("PerspectiveProjectionInterpolator", String.Format("list.Count = {0} : {1}, {2}, {3}, {4}"
-            //            , list.Count, source.X, source.Y, dest.X, dest.Y));
 
             return ret;
         }
