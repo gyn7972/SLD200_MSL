c553c2ba5c6f3802fb46a379432f654e2b640c8f
김영남 <ynkim@iqmc.co.kr>
2025-05-18 23:12:17 +0900

    UV 커밋

--- QMC.Common/Modules/WorkStage.cs
+++ QMC.Common/Modules/WorkStage.cs
@@ -40878,6 +40878,8 @@ namespace QMC.Common.Modules
             m_st4PointAlign_Result.dCenterOffsetY = dOffsetY;
             m_st4PointAlign_Result.dRotationAngle = 0;
 
+
+
             return m_st4PointAlign_Result;
         }
 
