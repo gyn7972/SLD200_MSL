440d403cd6fd93e6f4abd95a4b90460226ad2dcb
yngoo <yngoo@iqmc.co.kr>
2025-06-05 14:43:59 +0900

    스태커 오른쪽 포트 메시지 및 색상 수정
    `label_Title_Stacker_RPort`의 메시지와 색상 설정을
    `Equipment.Loader_RPort_Empty`를 사용하여 올바르게
    표시하도록 변경했습니다. 이로 인해 오른쪽 스태커의
    자재 유무에 대한 정보가 정확하게 반영됩니다.

--- SLD200_MSL/NewStyleForm/FormNew_Main.cs
+++ SLD200_MSL/NewStyleForm/FormNew_Main.cs
@@ -1323,8 +1323,8 @@ namespace SLD200_MSL
             SetValue(label_Title_Stacker_LPort,Equipment.Loader_LPort_Empty ? "Loader_Stacker Left : 자재 없음." : "Loader_Stacker Left: 자재 있음.");
             SetColor(label_Title_Stacker_LPort, Equipment.Loader_LPort_Empty ? Color.Red : Color.Black, Equipment.Loader_LPort_Empty ? Color.White : Color.Lime);
 
-            SetValue(label_Title_Stacker_RPort, Equipment.Loader_RPort_Empty ? "Loader_Stacker Right : 자재 없음." : "Loader_Stacker Left: 자재 있음.");
-            SetColor(label_Title_Stacker_LPort, Equipment.Loader_RPort_Empty ? Color.Red : Color.Black, Equipment.Loader_LPort_Empty ? Color.White : Color.Lime);
+            SetValue(label_Title_Stacker_RPort, Equipment.Loader_RPort_Empty ? "Loader_Stacker Right : 자재 없음." : "Loader_Stacker Right: 자재 있음.");
+            SetColor(label_Title_Stacker_RPort, Equipment.Loader_RPort_Empty ? Color.Red : Color.Black, Equipment.Loader_RPort_Empty ? Color.White : Color.Lime);
 
             //  소켓 가공 건너뛰기 (얼라인만 사용)
             SetColor(checkBox_Main_SocketDrilling_Pass, Equipment.SocketDrilling_Skip ? Color.LightGreen : Color.LightGreen);
