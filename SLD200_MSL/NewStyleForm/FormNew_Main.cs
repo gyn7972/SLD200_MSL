95e307d896017d136b232b9930a9d51990ff2052
yngoo <yngoo@iqmc.co.kr>
2025-06-11 21:10:27 +0900

    장비 정지 시 동작 추가 및 주석 업데이트
    `FormNew_Main.cs` 파일의 `SLD200_MSL` 네임스페이스에서
    장비 정지 시의 동작을 추가했습니다.
    - 버튼 색상 및 자동 실행 체크박스 상태 유지
    - 장비 정지 시 동작 설명 주석 추가
    - 장비 정지 상태 처리 메서드 호출 추가
    - 메인 틱 중지 설정 추가
    - 장비 운전 정지 시 상태 데이터 저장을 위한 주석 블록 추가

--- SLD200_MSL/NewStyleForm/FormNew_Main.cs
+++ SLD200_MSL/NewStyleForm/FormNew_Main.cs
@@ -2521,6 +2521,12 @@ namespace SLD200_MSL
             checkBox_Main_AutoRun.Checked = false;
 
 
+            // 장비 정지 시 그냥 정지 시킨다.
+            workStage.m_ScannerCameraOffsetSequence.Reset();
+            workStage.scannerCompensator.SetRunStatus(Part.RunStatus.Stop);
+            workStage.m_ScannerCameraOffsetSequence.m_MainTick_Start = false;
+
+
             ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
             //
             //  장비 운전 정지 시점의 모든 상태 데이터 저장
