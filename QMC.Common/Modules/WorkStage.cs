ad52fec4edf87f1a7ab1e2da9dd52e6f4d47bd15
yngoo <yngoo@iqmc.co.kr>
2025-07-12 20:18:05 +0900

    이미지 처리 기능 개선
    `WorkStage.cs` 파일의 `QMC.Common.Modules` 네임스페이스에 고해상도 카메라로 이미지를 가져오고, 이미지 너비에 따라 카운트를 설정하는 코드가 추가되었습니다. `bm_AlignRawData`에 최근 이미지를 저장하고, `Fiducial_aligner` 객체를 생성하여 원의 결과를 저장할 리스트를 초기화합니다. `bm_AlignRawData`가 null인 경우 카메라를 초기화하고 루프를 계속 진행하도록 설정하여 이미지 처리 및 피iducial 정렬 기능을 개선하였습니다.

--- QMC.Common/Modules/WorkStage.cs
+++ QMC.Common/Modules/WorkStage.cs
@@ -15454,6 +15454,19 @@ namespace QMC.Common.Modules
                                 //}
                                 //Thread.Sleep(200);
 
+
+                                // 여기서 이미지를 다시 가져와야지..
+                                Camera_HighRes.Grab();
+                                nWidthImageCount = (int)(dWidth / this.Config.ParamConfig.UpperVision_Scale_X);
+                                bm_AlignRawData = Camera_HighRes.LatestImage.RawData;
+                                Fiducial_aligner = new QMC_ImageProcessFindAlign();
+                                Fiducial_circlesResult = new List<RectangleF>();
+                                if (bm_AlignRawData == null)
+                                {
+                                    Camera_HighRes.Initialize();
+                                    continue;
+                                }
+
                                 // Circle Color 0: White, 1: Black
                                 if (mark.MarkColor <= 1)
                                 {
